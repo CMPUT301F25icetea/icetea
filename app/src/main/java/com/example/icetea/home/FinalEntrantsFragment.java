9305fd851d07597f4f8e2c9e936258b0ff3fd0cb
Haris Noor <harisnoor2@protonmail.com>
2025-11-25 00:51:00 -0700

    z

--- app/src/main/java/com/example/icetea/home/FinalEntrantsFragment.java
+++ app/src/main/java/com/example/icetea/home/FinalEntrantsFragment.java
@@ -9,6 +9,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.icetea.R;
 
@@ -45,6 +46,16 @@ public class FinalEntrantsFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_final_entrants, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
 
+        Button downloadCsvButton = view.findViewById(R.id.buttonDownloadCsv);
+
+        downloadCsvButton.setOnClickListener(v -> {
+            //eventId;
+        });
+
+    }
 }
